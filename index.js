a77d6cab5ba9489cc225da602c1735a6fcdedda8
trn-nam <khoanam19102006@gmail.com>
2022-10-30 11:11:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// function Search() {
+//         let input ul li filter
+//         input = document.getElementById('myInput')
+//         filter = input.value.toUpperCase();
+//         ul = document.getElementById('myList')
+//         li = ul.getElementsByTagName('li');
+
+
+//         for (let i = 0; i < li.length; i++) {
+//             var a = li[i].getElementsByTagName("a")[0];
+//             txtValue = a.TextContent ||a.innerText;
+//             if (txtValue.toUpperCase().indexOf(filter)> -1) {
+//                 li[i].style.display  = ""
+                
+//             }else{
+//                 li][i].style.display = "none";
+//             }
+            
+//         }
+//     }
+
